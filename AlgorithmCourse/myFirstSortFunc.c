ee8c00eea301783e73b60ca924df7a50f9b06497
DESKTOP-BSDN8H5\JM <mikey9610@naver.com>
2022-06-24 02:19:09 +0900

    DAY 0 : myFirstSortFunc - sample code

--- AlgorithmCourse/myFirstSortFunc.c
+++ AlgorithmCourse/myFirstSortFunc.c
@@ -2,10 +2,16 @@
 
 void myFirstSortFunc(int data[], int size) {
 
-	//--------------------------
-	//
-	//	write your code here
-	//
-	//--------------------------
+	// SAMPLE CODE
 
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j < size - i - 1; j++) {
+			int tmp;
+			if(data[j] > data[j + 1]) {
+				tmp = data[j];
+				data[j] = data[j + 1];
+				data[j + 1] = tmp;
+			}
+		}	
+	}
 }
